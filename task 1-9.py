dfbc119fe11c2279cec215e207b1182dedb074e1
Мария Речкалова <bykova.mary93@yandex.ru>
2025-04-15 21:27:31 +0500

    Второе задание

--- task 1-9.py
+++ task 1-9.py
@@ -8,7 +8,7 @@ class OnlineSalesRegisterCollector:
         self.__item_price = {'чипсы': 50, 'кола': 100, 'печенье': 45, 'молоко': 55, 'кефир': 70}
         self.__tax_rate = {'чипсы': 20, 'кола': 20, 'печенье': 20, 'молоко': 10, 'кефир': 10}
 
-    # 1 задание
+    #1 задание
     @property
     def name_items(self):
         return self.__name_items
@@ -16,4 +16,18 @@ class OnlineSalesRegisterCollector:
     @property
     def number_items(self):
         return self.__number_items
+    
+    #2 задание
+    def add_item_to_cheque(self, name):
+        try:
+            if len(name) == 0 or len(name) > 40:
+                raise ValueError(f'Нельзя добавить товар, если в его названии нет символов или их больше 40')
+            elif name not in self.__item_price:
+                raise NameError(f'Позиция отсутствует в товарном справочнике')
+            self.__name_items.append(name)
+            self.__number_items += 1
+        except ValueError as e:
+            print(e)
+        except NameError as e:
+            print(e)
 
